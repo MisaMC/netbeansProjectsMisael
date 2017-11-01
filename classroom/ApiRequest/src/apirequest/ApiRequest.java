c32325f6dd6e9c417a15ff7c7762f1709c83ccce
MisaMC <misael.munoz@alumno.uttehuacan.edu.mx>
2017-11-01 12:25:10 -0600

    Introduccion para leer Archivo Json

---
+++ classroom/ApiRequest/src/apirequest/ApiRequest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apirequest;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Usuario
+ */
+public class ApiRequest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        try {
+            URL url = new URL("https://api.darksky.net/forecast/5a02fd2308a169845bd02a8313c31f4d/18.45,-97.3833"); 
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept","application/json");
+            
+            if(conn.getResponseCode() != 200){//Si el resultado es diferente de 200, es un error
+            throw new RuntimeException("Failed: HTTP error code: "+ conn.getResponseCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));//Obtiene el archivo Json
+            
+            String output="";
+            while((output = br.readLine()) != null){
+                System.out.println(output);
+            }
+            
+            
+        } catch (MalformedURLException ex) {
+            JOptionPane.showMessageDialog(null,"Verificar: "+ ex.getMessage());
+        } catch (IOException ex) {
+           JOptionPane.showMessageDialog(null,"IOException"+ ex.getMessage());
+        }
+        
+    }
+    
+}
