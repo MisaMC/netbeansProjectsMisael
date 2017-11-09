83724d033f7f430d695efff9bafbd20924718c58
MisaMC <misael.munoz@alumno.uttehuacan.edu.mx>
2017-11-08 21:03:20 -0600

    Actualizacion

--- homework/Clima/src/clima/Clima.java
+++ homework/Clima/src/clima/Clima.java
@@ -3,27 +3,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package apirequest;
-import java.io.*;
+package clima;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-import org.json.simple.*;
-import org.json.simple.parser.*;
+
 /**
  *
  * @author Usuario
  */
-public class ApiRequest {
+public class Clima {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+    
         try {
             URL url = new URL("https://api.darksky.net/forecast/5a02fd2308a169845bd02a8313c31f4d/18.45,-97.3833"); 
             HttpURLConnection conn = (HttpURLConnection)url.openConnection();
@@ -36,26 +36,15 @@ public class ApiRequest {
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));//Obtiene el archivo Json
             
             String output="";
-            String jsonData = "";
-            JSONParser parser = new JSONParser();
             while((output = br.readLine()) != null){
-                //System.out.println(output);
-                jsonData += output;
+                System.out.println(output);
             }
-            Object object = parser.parse(jsonData);
-            JSONObject jObject = (JSONObject)object;
-            Double latitude = (Double)jObject.get("latitude");
-            Double longitude = (Double)jObject.get("longitude");
-            String timezone = (String)jObject.get("timezone");
-            System.out.println(latitude+" "+longitude+" "+timezone);
             
             
         } catch (MalformedURLException ex) {
             JOptionPane.showMessageDialog(null,"Verificar: "+ ex.getMessage());
         } catch (IOException ex) {
            JOptionPane.showMessageDialog(null,"IOException"+ ex.getMessage());
-        } catch (ParseException ex) {
-            JOptionPane.showMessageDialog(null,"Error " +ex.getMessage());
         }
         
     }
